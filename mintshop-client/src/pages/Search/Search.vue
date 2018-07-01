327430e6549bebcabef7f1701723675d61056f45
W-Qing <scorpio971028@gmail.com>
2018-07-02 00:44:44 +0800

    完成Search.vue静态页面

--- mintshop-client/src/pages/Search/Search.vue
+++ mintshop-client/src/pages/Search/Search.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Search vue template</div>
+  <section class="search">
+    <header class="header">
+      <div class="header_title">
+        <span class="header_title_text">搜索</span>
+      </div>
+    </header>
+    <form class="search_form" action="#">
+      <input type="search" name="search" placeholder="请输入商家或美食名称"
+    class="search_input">
+      <input type="submit" class="search_submit">
+    </form>
+  </section>
 </template>
 
 <script>
@@ -7,5 +18,60 @@ export default {}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixins.styl"
+  .search //搜索
+    width 100%
+    height 100%
+    .header //头部公共 css
+      background-color #02a774
+      position fixed
+      z-index 100
+      left 0
+      top 0
+      width 100%
+      height 45px
+      .header_search
+        position absolute
+        left 15px
+        top 50%
+        transform translateY(-50%)
+        width 10%
+        height 50%
+        .iconfont
+          font-size 22px
+          color #fff
+      .header_title
+        position absolute
+        top 50%
+        left 50%
+        transform translate(-50%, -50%)
+        width 30%
+        color #fff
+        font-size 22px
+        text-align center
+    .search_form
+      clearFix()
+      margin-top 45px
+      background-color #fff
+      padding 12px 8px
+      input
+        height 35px
+        padding 0 4px
+        border-radius 2px
+        font-weight bold
+        outline none
+        &.search_input
+          float left
+          width 79%
+          border 4px solid #f2f2f2
+          font-size 14px
+          color #333
+          background-color #f2f2f2
+        &.search_submit
+          float right
+          width 18%
+          border 4px solid #02a774
+          font-size 16px
+          color #fff
+          background-color #02a774
 </style>
