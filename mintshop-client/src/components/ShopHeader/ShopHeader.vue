be4e6a2d717befcc6c0543bffa5890770376e5bb
W-Qing <scorpio971028@gmail.com>
2018-08-06 16:03:19 +0800

    完成ShopHeader组件

--- mintshop-client/src/components/ShopHeader/ShopHeader.vue
+++ mintshop-client/src/components/ShopHeader/ShopHeader.vue
@@ -1,121 +1,134 @@
 <template>
   <div class="shop-header">
-    <nav class="shop-nav">
-      <a class="back">
+    <nav class="shop-nav" :style="{backgroundImage: `url(${info.bgImg})`}">
+      <a class="back" @click="$router.back()">
         <i class="iconfont icon-arrow_left"></i>
       </a>
     </nav>
-    <div class="shop-content">
-      <img class="content-image"
-        src="https://fuss10.elemecdn.com/8/40/02872ce8aefe75c16d3190e75ad61jpeg.jpeg">
+    <div class="shop-content" @click="toggleShopShow">
+      <img class="content-image" :src="info.avatar">
       <div class="header-content">
         <h2 class="content-title">
           <span class="content-tag">
             <span class="mini-tag">品牌</span>
           </span>
-          <span class="content-name">大鸭梨</span>
-        <i class="content-icon"></i>
-      </h2>
-      <div class="shop-message">
-        <span class="shop-message-detail">5</span>
-        <span class="shop-message-detail">月售 100 单</span>
-        <span class="shop-message-detail">
-          硅谷专送
-          <span>约 30 分钟</span>
-        </span>
-        <span class="shop-message-detail">距离 1000m</span>
-      </div>
-    </div>
-  </div>
-  <div class="shop-header-discounts">
-    <div class="discounts-left">
-      <div class="activity activity-green">
-        <span class="content-tag">
-          <span class="mini-tag">首单</span>
-        </span>
-        <span class="activity-content ellipsis">新用户下单立减 17 元</span>
-      </div>
-    </div>
-    <div class="discounts-right">
-      4 个优惠
-    </div>
-  </div>
-  <div class="shop-brief-modal" style="display: none">
-    <div class="brief-modal-content">
-      <h2 class="content-title">
-        <span class="content-tag">
-          <span class="mini-tag">品牌</span>
-        </span>
-        <span class="content-name">嘉禾一品（温都水城）</span>
-      </h2>
-      <ul class="brief-modal-msg">
-        <li><h3>3.5</h3><p>评分</p></li>
-        <li><h3>90 单</h3><p>月售</p></li>
-        <li><h3>硅谷专送</h3><p>约 28 分钟</p></li>
-        <li><h3>4 元</h3><p>配送费用</p></li>
-        <li><h3>1000m</h3><p>距离</p></li>
-      </ul>
-      <h3 class="brief-modal-title"><span>公告</span></h3>
-      <div class="brief-modal-notice">
-        是以粥为特色的中式营养快餐，自 2004 年 10 月 18 日创立“嘉和一品”品牌至今
-      </div>
-      <div class="mask-footer">
-        <span class="iconfont icon-close"></span>
-      </div>
-    </div>
-    <div class="brief-modal-cover"></div>
-  </div>
-  <div class="activity-sheet" style="display: none">
-    <div class="activity-sheet-content">
-      <h2 class="activity-sheet-title">优惠活动</h2>
-      <ul class="list">
-        <li class="activity-item activity-green">
-          <span class="content-tag">
-            <span class="mini-tag">首单</span>
+          <span class="content-name">{{info.name}}</span>
+          <i class="content-icon"></i>
+        </h2>
+        <div class="shop-message">
+          <span class="shop-message-detail">{{info.score}}</span>
+          <span class="shop-message-detail">月售{{info.sellCount}}单</span>
+          <span class="shop-message-detail">
+            {{info.description}}
+            <span>约{{info.deliveryTime}}分钟</span>
           </span>
-          <span class="activity-content">新用户下单立减 17 元(不与其它活动同享)</span>
-        </li>
-        <li class="activity-item activity-red">
-          <span class="content-tag">
-            <span class="mini-tag">满减</span>
-          </span>
-          <span class="activity-content">满 35 减 19，满 65 减 35</span>
-        </li>
-        <li class="activity-item activity-orange">
-          <span class="content-tag">
-            <span class="mini-tag">特价</span>
-          </span>
-          <span class="activity-content">【立减 19.5 元】欢乐小食餐</span>
-        </li>
-        <li class="activity-item activity-green">
-          <span class="content-tag">
-            <span class="mini-tag">首单</span>
-          </span>
-          <span class="activity-content">新用户下单立减 17 元(不与其它活动同享)</span>
-        </li>
-        <li class="activity-item activity-red">
-          <span class="content-tag">
-            <span class="mini-tag">满减</span>
-          </span>
-          <span class="activity-content">满 35 减 19，满 65 减 35</span>
-        </li>
-        <li class="activity-item activity-orange">
-          <span class="content-tag">
-            <span class="mini-tag">特价</span>
-          </span>
-          <span class="activity-content">【立减 19.5 元】欢乐小食餐</span>
-        </li>
-      </ul>
-        <div class="activity-sheet-close">
-          <span class="iconfont icon-close"></span>
+          <span class="shop-message-detail">距离{{info.distance}}</span>
         </div>
       </div>
-      <div class="activity-sheet-cover"></div>
     </div>
+    <div class="shop-header-discounts"  v-if="info.supports" @click="toggleSupportShow">
+      <div class="discounts-left">
+        <div class="activity" :class="supportClasses[info.supports[0].type]" >
+          <span class="content-tag">
+            <span class="mini-tag">{{info.supports[0].name}}</span>
+          </span>
+          <span class="activity-content ellipsis">{{info.supports[0].content}}</span>
+        </div>
+      </div>
+      <div class="discounts-right">
+      {{info.supports.length}}个优惠
+      </div>
+    </div>
+    <transition name="fade">
+      <div class="shop-brief-modal" v-show="shopShow">
+        <div class="brief-modal-content">
+          <h2 class="content-title">
+            <span class="content-tag">
+              <span class="mini-tag">品牌</span>
+            </span>
+            <span class="content-name">{{info.name}}</span>
+          </h2>
+          <ul class="brief-modal-msg">
+            <li>
+              <h3>{{info.score}}</h3>
+              <p>评分</p>
+            </li>
+            <li>
+              <h3>{{info.sellCount}}单</h3>
+              <p>月售</p>
+            </li>
+            <li>
+              <h3>{{info.description}}</h3>
+              <p>约{{info.deliveryTime}}分钟</p>
+            </li>
+            <li>
+              <h3>{{info.deliveryPrice}}元</h3>
+              <p>配送费用</p>
+            </li>
+            <li>
+              <h3>{{info.distance}}</h3>
+              <p>距离</p>
+            </li>
+          </ul>
+          <h3 class="brief-modal-title">
+            <span>公告</span>
+          </h3>
+          <div class="brief-modal-notice">
+            {{info.bulletin}}
+          </div>
+          <div class="mask-footer" @click="toggleShopShow">
+            <span class="iconfont icon-close"></span>
+          </div>
+        </div>
+        <div class="brief-modal-cover"></div>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="activity-sheet" v-show="supportShow">
+        <div class="activity-sheet-content">
+          <h2 class="activity-sheet-title">优惠活动</h2>
+          <ul class="list">
+            <li class="activity-item" v-for="(support, index) in info.supports"
+                :key="index" :class="supportClasses[support.type]">
+            <span class="content-tag">
+                <span class="mini-tag">{{support.name}}</span>
+              </span>
+              <span class="activity-content">{{support.content}}</span>
+            </li>
+          </ul>
+          <div class="activity-sheet-close" @click="toggleSupportShow">
+            <span class="iconfont icon-close"></span>
+          </div>
+        </div>
+        <div class="activity-sheet-cover"></div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+export default {
+  data () {
+    return {
+      supportClasses: ['activity-green', 'activity-red', 'activity-orange'],
+      shopShow: false,
+      supportShow: false
+    }
+  },
+  computed: {
+    ...mapState(['info'])
+  },
+  methods: {
+    toggleShopShow () {
+      this.shopShow = !this.shopShow
+    },
+
+    toggleSupportShow () {
+      this.supportShow = !this.supportShow
+    }
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
@@ -134,7 +147,6 @@
     height: 40px;
     padding: 5px 10px;
     position: relative;
-    background-image url("https://fuss10.elemecdn.com/f/5c/ead54394c3de198d3e6d3e9111bbfpng.png")
     &::before {
       content: '';
       position: absolute;
